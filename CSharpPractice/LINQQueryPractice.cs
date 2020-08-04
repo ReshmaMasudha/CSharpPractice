645d437381b301a7130408bc3e8144da57d7bc07
Reshma Masudha B <reshmarising1999@gmail.com>
2020-08-04 22:39:57 +0530

    updated commit

--- CSharpPractice/LINQQueryPractice.cs
+++ CSharpPractice/LINQQueryPractice.cs
@@ -10,15 +10,26 @@ namespace CSharpPractice
             //query dataSource
             int[] array = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
 
-            //query declaration
+            //query declaration query syntax
             var numQuery = ((
                 from num in array
                 where (num % 2) == 0
+                orderby num descending                                 //ordered by descending practice
                 select num).ToList());
+
+            //method Syntax
+            var Query = array.Where(num => num % 2 == 0).OrderBy(num => num);       //orderby is defaultly ascending
 
             foreach (var item in numQuery)
             {
-                Console.WriteLine(item);
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine(System.Environment.NewLine);
+
+            foreach (var item in Query)
+            {
+                Console.Write(item + " ");
             }
         }
     }
